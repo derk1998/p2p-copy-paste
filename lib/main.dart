e37dbab137877ce8f3c7ce3920b2a73d5f006dd2
Derk Wiegerinck <derkwiegerinck@gmail.com>
2024-05-22 14:22:10 +0200

    Disable firestore caching

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fd/flutter_fd.dart';
@@ -11,6 +12,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseFirestore.instance.settings =
+      const Settings(persistenceEnabled: false);
 
   runApp(P2PCopyPaste(
       navigator: NavigationManager(), systemManager: SystemManager()));
